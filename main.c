3a3d24f32d283338a8618520fdf6245ec9c95180
Samuel Šulo <80335420+s4mo-s@users.noreply.github.com>
2023-02-03 16:49:23 +0100

    Update main.c

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #include "esp_log.h"
 #include "esp_http_server.h"
 
-#define GPIO_LED_RED		2
+#define GPIO_LED_RED		 2
 #define GPIO_LED1		13
 #define GPIO_LED2		12
 #define GPIO_LED3		14
@@ -96,29 +96,29 @@ void set_options(led_options_t* options, int mode, int speed)
 /* TASK */
 void blink_task() {
 	while (1){
-        led_options_t options = {};
-        int cnt = 0;
+		led_options_t options = {};
+		int cnt = 0;
 
-        // Get structure from queue or waiting if queue is empty
-        while (1){
-            if(xQueueReceive(queue, &options, (TickType_t)(1000/portTICK_PERIOD_MS))){
-                break;
-            }
-            vTaskDelay(500/portTICK_PERIOD_MS);
-        }
+		// Get structure from queue or waiting if queue is empty
+		while (1){
+		    if(xQueueReceive(queue, &options, (TickType_t)(1000/portTICK_PERIOD_MS))){
+			break;
+		    }
+		    vTaskDelay(500/portTICK_PERIOD_MS);
+		}
 
-        // Process sequence
-        while(options.rounds) {
-            ESP_ERROR_CHECK(gpio_set_level(GPIO_LED1, cnt & options.led1));
-            ESP_ERROR_CHECK(gpio_set_level(GPIO_LED2, cnt & options.led2));
-            ESP_ERROR_CHECK(gpio_set_level(GPIO_LED3, cnt & options.led3));
-            ESP_ERROR_CHECK(gpio_set_level(GPIO_RED, cnt & options.red));
-            ESP_ERROR_CHECK(gpio_set_level(GPIO_GREEN, cnt & options.green));
-            ESP_ERROR_CHECK(gpio_set_level(GPIO_BLUE, cnt & options.blue));
-            vTaskDelay(options.speed / portTICK_RATE_MS);
-            options.rounds--;
-            cnt++;
-        }
+		// Process sequence
+		while(options.rounds) {
+		    ESP_ERROR_CHECK(gpio_set_level(GPIO_LED1, cnt & options.led1));
+		    ESP_ERROR_CHECK(gpio_set_level(GPIO_LED2, cnt & options.led2));
+		    ESP_ERROR_CHECK(gpio_set_level(GPIO_LED3, cnt & options.led3));
+		    ESP_ERROR_CHECK(gpio_set_level(GPIO_RED, cnt & options.red));
+		    ESP_ERROR_CHECK(gpio_set_level(GPIO_GREEN, cnt & options.green));
+		    ESP_ERROR_CHECK(gpio_set_level(GPIO_BLUE, cnt & options.blue));
+		    vTaskDelay(options.speed / portTICK_RATE_MS);
+		    options.rounds--;
+		    cnt++;
+		}
 	}
 }
 
@@ -133,12 +133,12 @@ void wifi_init(void)
 	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
 	wifi_config_t wifi_config = {
-		.ap = {
-            .ssid = ESP_WIFI_SSID,
-            .channel = ESP_WIFI_CHANNEL,
-            .password = ESP_WIFI_PASS,
-            .max_connection = MAX_STA_CONN,
-        },
+			.ap = {
+			    .ssid = ESP_WIFI_SSID,
+			    .channel = ESP_WIFI_CHANNEL,
+			    .password = ESP_WIFI_PASS,
+			    .max_connection = MAX_STA_CONN,
+        		},
 	};
 
 	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
@@ -159,10 +159,8 @@ esp_err_t post_handler(httpd_req_t *req){
 	size_t recv_size = MIN(req->content_len, sizeof(content));
 	int ret = httpd_req_recv(req, content, recv_size);
 
-	if (ret <= 0)
-	{
-		if (ret == HTTPD_SOCK_ERR_TIMEOUT)
-		{
+	if (ret <= 0){
+		if (ret == HTTPD_SOCK_ERR_TIMEOUT){
 			httpd_resp_send_408(req);
 		}
 		return ESP_FAIL;
@@ -186,15 +184,14 @@ esp_err_t post_handler(httpd_req_t *req){
 /* WEB SERVER */
 static httpd_handle_t start_webserver(void){
 	httpd_handle_t server = NULL;
-    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
-    config.uri_match_fn = httpd_uri_match_wildcard;
+    	httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    	config.uri_match_fn = httpd_uri_match_wildcard;
 
 	ESP_LOGI(TAG, "Starting HTTP Server");
 
 	esp_err_t ret = httpd_start(&server, &config);
 
-	if (ESP_OK != ret)
-	{
+	if (ESP_OK != ret){
 		ESP_LOGI(TAG, "Error starting server!");
 		return NULL;
 	}
@@ -204,61 +201,61 @@ static httpd_handle_t start_webserver(void){
 		.method = HTTP_GET,
 		.handler = get_handler,
 		.user_ctx ="<!DOCTYPE html>\
-                        <html>\
-                            <head>\
-                                <title>ESP32 controller</title>\
-                                <meta charset=\"utf-8\"/>\
-                                <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/>\
-                                <style>\
-                                    .button {\
-                                        border: none;\
-                                        border-radius: 15px;\
-                                        padding: 15px 30px;\
-                                        text-align: center;\
-                                        font-size: 30px;\
-                                        margin-top: 50px;\
-                                        cursor: pointer;\
-                                        color: white;\
-                                        background-color: blue;\
-                                    }\
-                                    body {\
-                                        font-family: Open Sans, Helvetica, Arial, sans-serif;\
-                                        font-size: 30px;\
-                                        text-align: center;\
-                                        background-color: #8fddff;\
-                                    }\
-                                    h1 {\
-                                        color: blue;\
-                                    }\
-                                    h2 {\
-                                        margin-bottom: 30px;\
-                                    }\
-                                    input, select {\
-                                        font-size: 4vh;\
-                                        margin-bottom: 10px;\
-                                    }\
-                                </style>\
-                            </head>\
-                            <body>\
-                                <h1>CONTROL</h1>\
-                                <form action=\"\" method=\"post\">\
-                                    <h2>Mode</h2>\
-                                    <select name=\"mode\">\
-                                        <option value=\"1\">1</option>\
-                                        <option value=\"2\">2</option>\
-                                        <option value=\"3\">3</option>\
-                                        <option value=\"4\">4</option></select\
-                                    ><br/>\
-                                    <h2>Speed</h2>\
-                                    <select name=\"speed\">\
-                                        <option value=\"1\">Slow</option>\
-                                        <option value=\"2\">Medium</option>\
-                                        <option value=\"3\">Fast</option></select\
-                                    ><br/>\
-                                    <input type=\"submit\" class=\"button\" value=\"Submit\"/>\
-                                </form>\
-                            </body>\
-                        </html>"
+				<html>\
+				    <head>\
+					<title>ESP32 controller</title>\
+					<meta charset=\"utf-8\"/>\
+					<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/>\
+					<style>\
+					    .button {\
+						border: none;\
+						border-radius: 15px;\
+						padding: 15px 30px;\
+						text-align: center;\
+						font-size: 30px;\
+						margin-top: 50px;\
+						cursor: pointer;\
+						color: white;\
+						background-color: blue;\
+					    }\
+					    body {\
+						font-family: Open Sans, Helvetica, Arial, sans-serif;\
+						font-size: 30px;\
+						text-align: center;\
+						background-color: #8fddff;\
+					    }\
+					    h1 {\
+						color: blue;\
+					    }\
+					    h2 {\
+						margin-bottom: 30px;\
+					    }\
+					    input, select {\
+						font-size: 4vh;\
+						margin-bottom: 10px;\
+					    }\
+					</style>\
+				    </head>\
+				    <body>\
+					<h1>CONTROL</h1>\
+					<form action=\"\" method=\"post\">\
+					    <h2>Mode</h2>\
+					    <select name=\"mode\">\
+						<option value=\"1\">1</option>\
+						<option value=\"2\">2</option>\
+						<option value=\"3\">3</option>\
+						<option value=\"4\">4</option></select\
+					    ><br/>\
+					    <h2>Speed</h2>\
+					    <select name=\"speed\">\
+						<option value=\"1\">Slow</option>\
+						<option value=\"2\">Medium</option>\
+						<option value=\"3\">Fast</option></select\
+					    ><br/>\
+					    <input type=\"submit\" class=\"button\" value=\"Submit\"/>\
+					</form>\
+				    </body>\
+				</html>"
 	};
 
 	const httpd_uri_t uri_post = {
@@ -266,61 +263,61 @@ static httpd_handle_t start_webserver(void){
 		.method = HTTP_POST,
 		.handler = post_handler,
 		.user_ctx ="<!DOCTYPE html>\
-                        <html>\
-                            <head>\
-                                <title>ESP32 controller</title>\
-                                <meta charset=\"utf-8\"/>\
-                                <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/>\
-                                <style>\
-                                    .button {\
-                                        border: none;\
-                                        border-radius: 15px;\
-                                        padding: 15px 30px;\
-                                        text-align: center;\
-                                        font-size: 30px;\
-                                        margin-top: 50px;\
-                                        cursor: pointer;\
-                                        color: white;\
-                                        background-color: blue;\
-                                    }\
-                                    body {\
-                                        font-family: Open Sans, Helvetica, Arial, sans-serif;\
-                                        font-size: 30px;\
-                                        text-align: center;\
-                                        background-color: #8fddff;\
-                                    }\
-                                    h1 {\
-                                        color: blue;\
-                                    }\
-                                    h2 {\
-                                        margin-bottom: 30px;\
-                                    }\
-                                    input, select {\
-                                        font-size: 4vh;\
-                                        margin-bottom: 10px;\
-                                    }\
-                                </style>\
-                            </head>\
-                            <body>\
-                                <h1>CONTROL</h1>\
-                                <form action=\"\" method=\"post\">\
-                                    <h2>Mode</h2>\
-                                    <select name=\"mode\">\
-                                        <option value=\"1\">1</option>\
-                                        <option value=\"2\">2</option>\
-                                        <option value=\"3\">3</option>\
-                                        <option value=\"4\">4</option></select\
-                                    ><br/>\
-                                    <h2>Speed</h2>\
-                                    <select name=\"speed\">\
-                                        <option value=\"1\">Slow</option>\
-                                        <option value=\"2\">Medium</option>\
-                                        <option value=\"3\">Fast</option></select\
-                                    ><br/>\
-                                    <input type=\"submit\" class=\"button\" value=\"Submit\"/>\
-                                </form>\
-                            </body>\
-                        </html>"
+				<html>\
+				    <head>\
+					<title>ESP32 controller</title>\
+					<meta charset=\"utf-8\"/>\
+					<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/>\
+					<style>\
+					    .button {\
+						border: none;\
+						border-radius: 15px;\
+						padding: 15px 30px;\
+						text-align: center;\
+						font-size: 30px;\
+						margin-top: 50px;\
+						cursor: pointer;\
+						color: white;\
+						background-color: blue;\
+					    }\
+					    body {\
+						font-family: Open Sans, Helvetica, Arial, sans-serif;\
+						font-size: 30px;\
+						text-align: center;\
+						background-color: #8fddff;\
+					    }\
+					    h1 {\
+						color: blue;\
+					    }\
+					    h2 {\
+						margin-bottom: 30px;\
+					    }\
+					    input, select {\
+						font-size: 4vh;\
+						margin-bottom: 10px;\
+					    }\
+					</style>\
+				    </head>\
+				    <body>\
+					<h1>CONTROL</h1>\
+					<form action=\"\" method=\"post\">\
+					    <h2>Mode</h2>\
+					    <select name=\"mode\">\
+						<option value=\"1\">1</option>\
+						<option value=\"2\">2</option>\
+						<option value=\"3\">3</option>\
+						<option value=\"4\">4</option></select\
+					    ><br/>\
+					    <h2>Speed</h2>\
+					    <select name=\"speed\">\
+						<option value=\"1\">Slow</option>\
+						<option value=\"2\">Medium</option>\
+						<option value=\"3\">Fast</option></select\
+					    ><br/>\
+					    <input type=\"submit\" class=\"button\" value=\"Submit\"/>\
+					</form>\
+				    </body>\
+				</html>"
 	};
 
 	// Set URI handlers
